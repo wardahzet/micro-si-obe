ba47ce1a1b74cce06ada827e9658c562703c95b0
Wardah Zahra <103308361+wardahzet@users.noreply.github.com>
2023-11-13 15:59:49 +0700

    Update CourseClassController.php

--- app/Http/Controllers/CourseClassController.php
+++ app/Http/Controllers/CourseClassController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class CourseClassController extends Controller
 {
-    //
+    public function getClassesbyCourseName($courseName)
+    {
+        //
+    }
+
+    public function getClassesbyCourseId($courseId)
+    {
+        //
+    }
+
+    public function storeClass(Request $request)
+    {
+        //
+    }
 }
