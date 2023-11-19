1b9fda58445809c86bef457ca2b7117520cea5b5
ALIFIA PUTRI BUDIYANTI <asiyah_xav@student.ub.ac.id>
2023-11-19 21:06:44 +0700

    join class

---
+++ app/Http/Controllers/JoinClassController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JoinClass;
+use Illuminate\Http\Request;
+
+class JoinClassController extends Controller
+{
+    public function index()
+    {
+        $joinClass = JoinClass::all();
+
+        return response()->json([
+            'success'=> true,
+            'data'=>$joinClass
+        ]);
+    }
+
+    public function show(Request $request)
+    {
+        $courseClassId = $request->course_class_id;
+
+        // Menghitung jumlah mahasiswa dengan course_class_id yang sama
+        $studentCount = JoinClass::where('course_class_id', $courseClassId)->count();
+
+        // Mengambil data JoinClass dengan relasi CourseClass
+        $joinClass = JoinClass::with(['courseClass'])
+            ->where('course_class_id', $courseClassId)
+            ->first();
+
+        if (!$joinClass) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Data tidak ditemukan',
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'class' => $joinClass->course_class_id,
+                'class_code' => $joinClass->courseClass->class_code,
+                'class_name' => $joinClass->courseClass->name,
+                'course_id' => $joinClass->courseClass->course_id,
+                'student_count' => $studentCount,
+            ],
+        ]);
+    }
+    
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'course_class_id' => 'required',
+            'student_user_id' => 'required',
+        ]);
+
+        $joinClass = JoinClass::create([
+            "course_class_id" => $request->course_class_id,
+            "student_user_id" => $request->student_user_id,
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data berhasil diinputkan',
+            'data' => [
+                'joinClass' => $joinClass
+            ]
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JoinClassController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return ('welcome');
+});
+
