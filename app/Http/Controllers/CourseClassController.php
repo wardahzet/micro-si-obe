559708920d3174958564232333d3832302704c84
betgirs <elisabetgirsang@student.ub.ac.id>
2023-11-20 17:44:22 +0700

    get all class & buat class ygy

--- app/Http/Controllers/CourseClassController.php
+++ app/Http/Controllers/CourseClassController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CourseClass;
 use Illuminate\Http\Request;
 
 class CourseClassController extends Controller
@@ -16,8 +17,40 @@ class CourseClassController extends Controller
         //
     }
 
-    public function storeClass(Request $request)
+    public function create(Request $request)
     {
-        //
+       
+            
+            $CourseClass = CourseClass::create([
+                'course_id'=> $request->course_id,
+                'name'=> $request->name,
+               // 'thumbnail_img'=> $request->thumbnail_img,
+                'class_code'=> $request->class_code,
+                'creator_user_id'=> $request->creator_user_id,
+                'syllabus_id'=> $request-> syllabus_id,
+               // 'settings'=> $request->settings,
+            ]);
+    
+            return response()->json([
+                'status' => 'Success',
+                'message' => 'new class created',
+                'data' => [
+                    'class' => $CourseClass,
+                ]
+                ], 200);
+    }
+
+
+    public function getAllClass()
+    {
+        $CourseClass = CourseClass::all();
+
+        return response()->json([
+            'status' => 'Success',
+            'message' => 'All class grabbed',
+            'data'=>[
+                'classes' => $CourseClass,
+            ]
+            ],200);
     }
 }
