3326558bb0a2ab5c6bf223aa25bd8fea46aaaacd
Wardah Zahra <103308361+wardahzet@users.noreply.github.com>
2023-11-26 16:58:12 +0700

    Update CourseClassController.php

--- app/Http/Controllers/CourseClassController.php
+++ app/Http/Controllers/CourseClassController.php
@@ -4,11 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\JoinClass;
 use App\Models\CourseClass;
-use App\Models\JoinClass;
-use App\Models\CourseClass;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
@@ -33,62 +29,8 @@ class CourseClassController extends Controller
                 'error' => $e->getMessage(),
             ]);
         }
-    //fungsi delete class
-    public function deleteClass($id)
-    {
-        try {
-            DB::transaction(function () use ($id){
-                $courseClass = CourseClass::find($id);
-                
-                JoinClass::destroy(collect($courseClass->join_classes)->pluck('id'));
-                $courseClass->delete();
-            });
-            return response()->json([
-                'status' => 'success',
-            ]);
-        } catch (\Exception $e) {
-            return response()->json([
-                'status' => 'error',
-                'error' => $e->getMessage(),
-            ]);
-        }
     }
 
-    public function create(Request $request)
-    {
-       
-            
-            $CourseClass = CourseClass::create([
-                'course_id'=> $request->course_id,
-                'name'=> $request->name,
-               // 'thumbnail_img'=> $request->thumbnail_img,
-                'class_code'=> $request->class_code,
-                'creator_user_id'=> $request->creator_user_id,
-                'syllabus_id'=> $request-> syllabus_id,
-               // 'settings'=> $request->settings,
-            ]);
-    
-            return response()->json([
-                'status' => 'Success',
-                'message' => 'new class created',
-                'data' => [
-                    'class' => $CourseClass,
-                ]
-                ], 200);
-    }
-
-
-    public function getAllClass()
-    {
-        $CourseClass = CourseClass::all();
-
-        return response()->json([
-            'status' => 'Success',
-            'message' => 'All class grabbed',
-            'data'=>[
-                'classes' => $CourseClass,
-            ]
-        ]);
     public function getClassesbyCourseId($courseId)
     {
         //
