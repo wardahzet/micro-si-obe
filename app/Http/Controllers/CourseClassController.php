113a8d4f6fe790c1f1202a7792374b37ec0f2e25
Wardah Zahra <103308361+wardahzet@users.noreply.github.com>
2023-11-26 17:25:16 +0700

    update

--- app/Http/Controllers/CourseClassController.php
+++ app/Http/Controllers/CourseClassController.php
@@ -31,11 +31,6 @@ class CourseClassController extends Controller
         }
     }
 
-    public function getClassesbyCourseId($courseId)
-    {
-        //
-    }
-
     public function create(Request $request)
     {
        
@@ -71,5 +66,38 @@ class CourseClassController extends Controller
                 'classes' => $CourseClass,
             ]
             ],200);
+    }
+
+    public function getClassesbyCourseName($courseName)
+    {
+        //
+        try {
+            $courses = CourseClass::where('name', 'LIKE', "%$courseName%")->get();
+            if ($courses->isEmpty()) {
+                return response()->json(['error' => 'Course tidak ditemukan'], 404);
+            }
+            $responseData = [
+                'course_classes' => $courses->toArray()
+            ];
+            return response()->json($responseData, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+    public function getClassesbyCourseId($courseId)
+    {
+        //
+        try {
+            $courses = CourseClass::where('course_id', $courseId)->get();
+            if ($courses->isEmpty()) {
+                return response()->json(['error' => 'Course tidak ditemukan'], 404);
+            }
+            $responseData = [
+                'course_classes' => $courses->toArray()
+            ];
+            return response()->json($responseData, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
